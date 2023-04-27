9bdb3ac9a91c1f9f7dae75d0b5b1b919115e07b7
naushinhoque <fnmh@yahoo.com>
2023-04-27 18:06:17 -0500

    done

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -4,8 +4,8 @@ import React from 'react';
 const NasaPhoto = (props) => {
     return (
         <div className="nasa-photo-wrapper">
-            <h3>{props.photo.title}</h3>
-            <p>{props.photo.date}</p>
+            <h3 className="title">{props.photo.title}</h3>
+            <p className="date">{props.photo.date}</p>
             <img src={props.photo.hdurl}></img>
             <p className="explanation">{props.photo.explanation}</p>
         </div>
